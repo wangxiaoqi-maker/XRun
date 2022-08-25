f91577a561a1ada33dfe16f87148050f1a3552b8
wangxiaoqi-maker <17621525387@139.com>
2022-08-25 10:38:40 +0800

    修改url名称

--- apps/Configs/urls.py
+++ apps/Configs/urls.py
@@ -4,10 +4,10 @@ from rest_framework import routers
 from Configs import views
 
 urlpatterns = [
-    path('createrCofigs', views.ConfigsView.as_view({'post': 'create'})),
-    path('getConfigs', views.ConfigsView.as_view({'get': 'list'})),
-    path('getConfigsDetail', views.ConfigsView.as_view({'get': 'retrieve'})),
-    path('updateConfigs', views.ConfigsView.as_view({'post': 'update'})),
-    path('delConfigs', views.ConfigsView.as_view({'post': 'destroy'})),
+    path('createrHosts', views.ConfigsView.as_view({'post': 'create'})),
+    path('getHosts', views.ConfigsView.as_view({'get': 'list'})),
+    path('getHostsDetail', views.ConfigsView.as_view({'get': 'retrieve'})),
+    path('updateHosts', views.ConfigsView.as_view({'post': 'update'})),
+    path('delHosts', views.ConfigsView.as_view({'post': 'destroy'})),
 
 ]
