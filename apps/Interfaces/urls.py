864026650cc5e2d599361686892b3fa95951fa96
wangxiaoqi-maker <17621525387@139.com>
2022-09-01 15:08:01 +0800

    更改接口项目的url

--- apps/Interfaces/urls.py
+++ apps/Interfaces/urls.py
@@ -4,10 +4,10 @@ from rest_framework import routers
 from Interfaces import views
 
 urlpatterns = [
-    path('createrInterfaces', views.InterfacesView.as_view({'post': 'create'})),
-    path('getInterfaces', views.InterfacesView.as_view({'get': 'list'})),
-    path('getInterfacesDetail', views.InterfacesView.as_view({'get': 'retrieve'})),
-    path('updateInterfaces', views.InterfacesView.as_view({'post': 'update'})),
-    path('delInterfaces', views.InterfacesView.as_view({'post': 'destroy'})),
+    path('createrApi', views.InterfacesView.as_view({'post': 'create'})),
+    path('getApi', views.InterfacesView.as_view({'get': 'list'})),
+    path('getApiDetail', views.InterfacesView.as_view({'get': 'retrieve'})),
+    path('updateApi', views.InterfacesView.as_view({'post': 'update'})),
+    path('delApi', views.InterfacesView.as_view({'post': 'destroy'})),
 
 ]
