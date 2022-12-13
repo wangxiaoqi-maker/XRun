910431e4f56be0fbe183fc006ae0d37dc7045ab6
wangxiaoqi-maker <17621525387@139.com>
2022-12-13 10:09:05 +0800

    1.添加定时任务模块

--- gm_api_automation/settings.py
+++ gm_api_automation/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django_apscheduler',
+    'django_apscheduler',
     'rest_framework',
     'django_filters',
     'Projects',
